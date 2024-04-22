47b3bdb727950051ec98f7809f6920cbc0a31c51
NisalDeZoysa <90953837+NisalDeZoysa@users.noreply.github.com>
2024-04-23 00:05:55 +0530

    pipeline file

---
+++ pipeline.groovy
@@ -0,0 +1,23 @@
+pipeline {
+  agent any
+  triggers{
+    githubPush()
+  }
+  stages {
+    stage('build'){
+      steps {
+        sh 'docker build -t thetharz/3996-node-app-image .'
+      }
+    }
+    stage('run'){
+      steps{
+        sh 'docker run -d -p 5000:3000 thetharz/3996-node-app-image'
+      }
+    }
+    stage('status'){
+      steps{
+        sh 'docker ps'
+      }
+    }
+  }
+}
